b4529ad21316095595b12818739e50719c8cec3e
woiic <40223167+woiic@users.noreply.github.com>
2018-06-20 15:57:25 -0400

    funciones cliente

--- Server/Server prueba/Cient_test.py
+++ Server/Server prueba/Cient_test.py
@@ -1,15 +1,32 @@
 import socket
 from Tkinter import*
 from time import*
-import random 
+import funciones as fun
 
 tinicial= int(time())
 
 host = '172.29.1.1'
-port = 8888
+port = 10003
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((host, port))
+
+
+def divide(x):
+  r = x.split('/')
+  return r
+
+def element1(x):
+  r = x[1:len(x)]
+  return r
+
+def element2(x,c):
+  r = x[c]
+  return r
+
+def element3(x):
+  r = x[0:len(x)-1]
+  return r
 
 
 def fd():
@@ -17,25 +34,43 @@ def fd():
   
 def left():
   s.send(str.encode("Lf."))
+  return
   
 def staph():
   s.send(str.encode("St."))
+  return
   
 def right():
   s.send(str.encode("Rt."))
+  return
   
 def bd():
   s.send(str.encode("Bd."))
+  return
+
+def cl():
+  s.send(str.encode("CLOSE"))
+  return
+
+def kl():
+  s.send(str.encode("KILL"))
+  return
+
   
 def prender1():
      global pto
      pto = 1
+     return
      
 def apagar1():
      global pto
-     pto = 0          
+     pto = 0
+     return
 
 vent = Tk()
+
+def sd():
+  vent.kill
 
 global pto 
 pto = 0
@@ -66,7 +101,7 @@ Bd = Button(marco3, width=20,text="Atras",command=bd)
 Bd.pack()
 
 marco4 = Frame(vent)
-marco4.pack(side=LEFT)
+marco4.pack()
 
 si = Button(marco4, text="Prender",command=prender1)
 si.pack(side=LEFT)
@@ -80,22 +115,29 @@ acelerometro.pack(side=LEFT)
 acce = Label(marco4, text="      ")
 acce.pack(side=LEFT)
 
-marco6 = Frame(vent)
-marco6.pack()
-
-temperatura = Label(marco6, text="Temperatura=" )
+temperatura = Label(marco4, text="Temperatura =" )
 temperatura.pack(side=LEFT)
 
-temper = Label(marco6,text="      ")
+temper = Label(marco4,text="      ")
 temper.pack(side=LEFT)
 
-hora = Label(marco6, text="Tiempo =" )
+hora = Label(marco4, text="Tiempo =" )
 hora.pack(side=LEFT)
 
-rolex = Label(marco6, text="")
+rolex = Label(marco4, text="")
 rolex.pack(side=LEFT)
 
+admin = Frame(vent)
+admin.pack()
 
+close = Button(admin, width=20, text="CLOSE", command=cl)
+close.pack(side=LEFT)
+
+kill = Button(admin, width=20, text="KILL", command=kl)
+kill.pack(side=LEFT)
+
+shut_down = Button(admin, width=20, text="SHUT DOWN", command=sd)
+shut_down.pack()
 
 def add_time():
   tiempo = str( int(time()) - tinicial -1)
@@ -107,12 +149,21 @@ def add_time():
 def sensor():
   s.send(str.encode("Se."))
   men = s.recv(1024)
-  print(men.decode('utf-8'))
+  data = divide(men)
+  acce = element2(data,0)
+  acce = element1(acce)
+  print acce
+  estado = element2(data,1)
+  print estado
+  temp = element2(data,2)
+  temp = element3(temp)
+  print temp
   
   vent.after(3000, sensor)
+
   
 vent.after(0, add_time)  
-vent.after(0, sensor)
+vent.after(1000, sensor)
 vent.mainloop()
 print("se ha cerrado la ventana")
 
---
+++ Server/Server prueba/funciones.py
@@ -0,0 +1,11 @@
+
+
+def divide(x):
+  r = x.split('/')
+  return r
+
+
+def element(x,c):
+  r = x[c]
+  return r
+
