26e3f5c5dc7ffd020621cb85b12fcc956a53a48f
kawallala <martin.araya.zavala@gmail.com>
2018-06-27 14:26:21 -0400

    limpieza de las carpetas

--- ARDUINO_Final/ARDUINO_Final.ino
+++ ARDUINO_Final/ARDUINO_Final.ino
@@ -11,43 +11,37 @@ Control de motores de CC con CI L298
 
 #define MOTOR_CTR1  5   // I1 Input 2
 #define MOTOR_PWM1  6  //  
-#define MOTOR_CTR2  7 //    I2 Input 2
-
- 
+#define MOTOR_CTR2  7 //    I2 Input 2 
 
 #define MOTOR_DIR_FORWARD  0   // Adelante
 #define MOTOR_DIR_BACKWARD  1  // Atras
 
 #define MOTOR_DIR_RIGHT  0   // Adelante
 #define MOTOR_DIR_LEFT  1  // Atras
-int v=255;
+
+int v=255; //velocidad
 
 String inputString = "";       
 boolean stringComplete = false;
 String c;
+String msj; //mensaje pi
+int Status = 5; //Estado de motores
 
-int x, y, z;
+int acelx, acely, acelz;  //aceleraciones por eje
 
 void setup(){
-   // Configuracion de pines para control del motor
-   
+   // Configuracion de pines para control del motor   
    // Control de sentido de giro
    pinMode(MOTOR_CTL1,OUTPUT);
    pinMode(MOTOR_CTL2,OUTPUT);
    // Control de velocidad
    pinMode(MOTOR_PWM,OUTPUT);
-
    // Control de sentido de giro del motor 2
    pinMode(MOTOR_CTR1,OUTPUT);
    pinMode(MOTOR_CTR2,OUTPUT);
    pinMode(MOTOR_PWM1,OUTPUT);
-   Serial.begin(9600);
-   
-   //
-   inputString.reserve(200);
-   Serial.println('xx,yy,zz');
-   
-   
+   Serial.begin(115200);   
+   inputString.reserve(200);   
 }
 
 // Control de velocidad mediante PWM
@@ -95,8 +89,7 @@ void motorMove(boolean direction)
      case MOTOR_DIR_FORWARD:
      {
        digitalWrite(MOTOR_CTL1,LOW);
-       digitalWrite(MOTOR_CTR1,HIGH);
-       
+       digitalWrite(MOTOR_CTR1,HIGH);       
        digitalWrite(MOTOR_CTL2,HIGH);  
        digitalWrite(MOTOR_CTR2,LOW);  
               
@@ -106,8 +99,7 @@ void motorMove(boolean direction)
      case MOTOR_DIR_BACKWARD:
      {
         digitalWrite(MOTOR_CTL1,HIGH);
-        digitalWrite(MOTOR_CTR1,LOW);
-        
+        digitalWrite(MOTOR_CTR1,LOW);        
         digitalWrite(MOTOR_CTL2,LOW); 
         digitalWrite(MOTOR_CTR2,HIGH);         
      }
@@ -139,80 +131,68 @@ void loop(){
   
   setSpeed(v);
   
-  if (stringComplete){
-    
-    
-    c = inputString.substring(0,2);
-    
+  if (stringComplete){    
+    c = inputString.substring(0,2);    
     if (c=="Fd"){
       motorFree();
       delay(100);
       motorMove(MOTOR_DIR_FORWARD);
-      Serial.println("Fd");
+      //Serial.println("Fd");
+      Status = 8;
     }
     else if (c=="Bd"){
       motorFree();
       delay(100);
-      motorMove(MOTOR_DIR_BACKWARD);;
-      Serial.println("Bd");
+      motorMove(MOTOR_DIR_BACKWARD);
+      //Serial.println("Bd");
+      Status = 2;
     }
     else if(c=="Lf"){
       motorFree();
       delay(100);
       turnAngle(MOTOR_DIR_LEFT);
-      Serial.println("Lf");
+      //Serial.println("Lf");
+      Status = 4;
     }
     else if(c=="Rt"){
       motorFree();
       delay(100);
       turnAngle(MOTOR_DIR_RIGHT);
-      Serial.println("Rt");
+      //Serial.println("Rt");
+      Status = 6;
     }
     else if(c=="St"){
       motorStop();  
-      Serial.println("St");
+      //Serial.println("St");
+      Status = 5;
     }
-    else if(c == "AC"){
-      
-      //Acelerometro
-      
-      x = analogRead(0);       // read analog input pin 0
-      y = analogRead(1);       // read analog input pin 1
-      z = analogRead(2);      // read analog input pin 1
-      float xx= map(x,0,1023,45.0,-45.0);
-      float yy= map(y,0,1023,45.0,-45.0);
-      float zz= map(z,0,1023,45.0,-45.0);
-      String a;
-      a = '[' + String(int(xx)) + 'X' + ',' + String(int(yy)) + 'Y' + ',' + String(int(zz)) + 'Z' +']' ; 
-      Serial.println(a); 
-      
-    }
-    else if(c == "TP"){
-      int tempC; String temp;
-      tempC = analogRead(3);   
-      tempC = (5.0 * tempC * 100.0)/1024.0;
-      temp = '[' + String(tempC) + ']' ;
-     Serial.println(temp);
-    }
-    
+    else if(c == "Se"){      
+      //Acelerometro      
+      acelx = analogRead(0);
+      acelx = analogRead(0);      
+      acely = analogRead(1); 
+      acely = analogRead(1);      
+      acelz = analogRead(2);      
+      acelz = analogRead(2);           
+      //temperatura      
+      int tempC;
+      tempC = analogRead(3);      
+      msj = '[' + String(acelx) + ';' + String(acely) + ';' + String(acelz) + '/' + String(Status) + '/' + String(tempC) + ']' ; 
+      Serial.print(msj);      
+    }    
     inputString = "";
     c = "";
     stringComplete = false;
-  }
-  
+  }  
 }
 
 
 void serialEvent() {
-  while (Serial.available()) {
-    
+  while (Serial.available()) {    
     char inChar = (char)Serial.read();
-    inputString += inChar;
-    
-    if (inChar == '.'){
-      
-      stringComplete = true;
-      
+    inputString += inChar;    
+    if (inChar == '.'){      
+      stringComplete = true;      
     }
   }
 }
