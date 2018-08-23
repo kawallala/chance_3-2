36287e8aef9de08551f43af912d58dd24009f6cf
kawallala <martin.araya.zavala@gmail.com>
2018-08-23 15:34:30 -0300

    embelleciendo codigos

--- Server_cambio.py
+++ Server_cambio.py
@@ -1,13 +1,13 @@
 # coding=utf-8
+
 # importamos librerias necesarias
 import socket
 from serial import *
 
 # variables necesarias para la iniciacion
 host = ''
-port = 10001
-ser = Serial('/dev/ttyUSBO', 115200)
-dict = { 8 : 'Fwd.' , 5 : 'Sto.',  2: 'Bwd.' , 4:'Let.' , 6:'Rit.'} 
+port = 10000
+ser = Serial('/dev/ttyUSB0', 115200)
 data_old = ['5',"90","12"]
 
 def setup_server():
@@ -29,9 +29,7 @@ def setup_server():
 
 def setup_connection(s):
     """
-    stupConnection(): None -> conn
-
-    
+    setupConnection(): socket -> conn    
 
     Metodo para aceptar una conexion de un cliente, devuelve la direccion de la
     conexion
@@ -60,6 +58,12 @@ def read_serial():
     return b
 
 def dataCompare(data1,data2):
+    """
+    dataCompare(): list list -> None
+
+    Metodo para comparar el cambio entre la lista guardada y la lista recibida por el cliente
+    en caso de cambio, se envia el mensaje correspondiente a travez del puerto Serial
+    """
     for i in range(len(data1)):
         if data1[i] != data2[i]:
             if i == 0:
@@ -86,9 +90,6 @@ def dataCompare(data1,data2):
                 print 'v' + data1[2] + '.'
                 ser.write('v' + data1[2] + '.')
                 
-
-
-    
 def data_transfer(conn):
     """
     dataTransfer(conn): conn -> None
@@ -96,20 +97,18 @@ def data_transfer(conn):
     Metodo que recibe la conexion realizada anterormente e inicializa la comunicacion con la misma, esperando a
     recibir un mensaje, se escribe en el serial el codigo correspondiente para la acción requerida
     """
-    # lo que hay aqui recibe y envia datos
     while True:
-        global data_old
-        # recibo datos
-        data = conn.recv(1024)
-        data = data.split('/')
-        #print data
-        if data == ['']:
+        global data_old   
+        data = conn.recv(1024) #recibir datos
+        data = data.split('/') #separar segun slash, creando una lista
+        if data == [''] or type(data) != list or len(data) != 3: # si el mensaje esta vacio, no es del tipo correspondiente, o es del largo incorrecto,
+                                                                  # debido a un error en el cliente, se detienene el robot y el servidor se cierra
             ser.write('Sto.')
             print 'Cliente perdido, Cerrando servidor'
             conn.close()
             s.close()
             break
-        elif data == ['CLOSE']:
+        elif data == ['CLOSE']: #si el cliente envia el mensaje de terminacion, se detienene el robot y se cierra el servidor
             ser.write('Sto.')
             print 'Cliente desconectado, Cerrando servidor'
             conn.close()
@@ -118,7 +117,6 @@ def data_transfer(conn):
         else:
             dataCompare(data,data_old)
             a=read_serial()
-            #print a
             conn.sendall(a)                       
             data_old = data
         print "Data Sent"
@@ -132,14 +130,8 @@ def setup_all():
     conn = setup_connection(s)
     data_transfer(conn)
 
-
-def go():
-    """
-    Loop principal, donde se realizan todas las acciones
-    """
-    while True:
-        try:
-            setup_all()
-        except:
-            break
-go()
+while True:
+    try:
+        setup_all()
+    except:
+        break
