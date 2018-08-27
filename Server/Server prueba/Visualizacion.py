b14ba81231bb45ebc3ec8a703d43773f75b99a45
kawallala <martin.araya.zavala@gmail.com>
2018-08-27 15:19:07 -0300

    pruebas y arreglos visualizacion

--- Server/Server prueba/Visualizacion.py
+++ Server/Server prueba/Visualizacion.py
@@ -8,24 +8,42 @@ pos = []
 accx = []
 accy = []
 accz = []
-
 T = []
-
+t = []
 for line in datos:
     line = line[1:len(line)-2]
     line = line.split('/')
 
     pos.append(line[1])
     acceleraciones = line[0].split(';')
+    acceleraciones[0] = -0.15 * int(acceleraciones[0]) + 52
+    acceleraciones[1] = -0.15 * int(acceleraciones[1]) + 50.01
+    acceleraciones[2] = -0.14 * int(acceleraciones[2]) + 47.7
     accx.append(acceleraciones[0])
     accy.append(acceleraciones[1])
     accz.append(acceleraciones[2])
-    T.append(line[2])
+    T.append(float(line[2]))
+    t.append(float(line[4]))
 
-t = [t for t in range(len(accx))]
+maxacc=max([max(accx), max(accy), max(accz)])
+minacc=min([min(accx), min(accy), min(accz)])
+tmin = min(t)
+tmax= max(t)
+
+
+
+plt.figure(1,figsize=(6,3))
+plt.subplot(121)
+plt.axis((tmin,tmax,maxacc,minacc))
 plt.plot(t , accx)
 plt.plot(t , accy)
 plt.plot(t , accz)
 plt.xlabel('Tiempo [s]')
 plt.ylabel('acceleracion eje x [m/s^2]')
+plt.title('Aceleraciones')
+plt.subplot(122)
+plt.plot(t,T)
+plt.xlabel('Tiempo[s]')
+plt.ylabel('Temperatura [C]')
+plt.title('Temperaturas')
 plt.show()
--- Server/Server prueba/metodo martin.py
+++ Server/Server prueba/metodo martin.py
@@ -1,4 +1,4 @@
-## Librerias utilizadas para el funcionamientoo del reobot ##
+## Librerias utilizadas para el funcionamientoo del robot ##
 
 import socket  ## Nos permite conectarnos por internet ##
 import Tkinter as Tk  ## Nos permite crear una ventana para la GUI ##
@@ -14,7 +14,7 @@ tinicial = int(time.time())
 
 
 ## La direccion ip de la raspberry pi, y el ppuerto por el que nos conectamos ##
-host = '192.168.43.46'
+host = '172.30.1.1'
 port = 10001
 
 
@@ -211,6 +211,9 @@ def sensor():
     #print data
     acceleration = data[0].split(';')
     print acceleration
+    acceleration[0] = str(-0.15*int(acceleration[0])+52)
+    acceleration[1] = str(-0.15*int(acceleration[1])+50.01)
+    acceleration[2] = str(-0.14*int(acceleration[2])+47.7)
     acceleration = acceleration[0] + " X - " + acceleration[1] + " Y - " + acceleration[2] + " Z"
     accel.config(text=str(acceleration))
 
